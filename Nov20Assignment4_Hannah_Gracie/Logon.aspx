1ffb7d10e6a93acca52b3a5d935cf3a147091e93
oksanamyr <Oksana.Myronovych@ndsu.edu>
2023-11-20 21:17:51 -0600

    log and links

--- Nov20Assignment4_Hannah_Gracie/Logon.aspx
+++ Nov20Assignment4_Hannah_Gracie/Logon.aspx
@@ -1,3 +1,22 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Logon.aspx.cs" Inherits="Nov20Assignment4_Hannah_Gracie.Logon" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <br />
+        <asp:Login ID="Login1" runat="server" BackColor="#FFFBD6" BorderColor="#FFDFAD" BorderPadding="4" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#333333" OnAuthenticate="Login1_Authenticate" style="font-size: x-large" TextLayout="TextOnTop">
+            <InstructionTextStyle Font-Italic="True" ForeColor="Black" />
+            <LoginButtonStyle BackColor="White" BorderColor="#CC9966" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#990000" />
+            <TextBoxStyle Font-Size="0.8em" />
+            <TitleTextStyle BackColor="#990000" Font-Bold="True" Font-Size="0.9em" ForeColor="White" />
+        </asp:Login>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
 </asp:Content>
--- Nov20Assignment4_Hannah_Gracie/Logon.aspx.cs
+++ Nov20Assignment4_Hannah_Gracie/Logon.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,8 +10,64 @@ namespace Nov20Assignment4_Hannah_Gracie
 {
     public partial class Logon : System.Web.UI.Page
     {
+
+        KarateDataContext dbcon;
+        string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Oksan\\source\\repos\\Nov20Assignment4_Hannah_Gracie\\Nov20Assignment4_Hannah_Gracie\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            dbcon=new KarateDataContext(conn);
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string nUserName = Login1.UserName;
+            string nPassword = Login1.Password;
+
+
+            HttpContext.Current.Session["nUserName"] = nUserName;
+            HttpContext.Current.Session["uPass"] = nPassword;
+
+
+
+            // Search for the current User, validate UserName and Password
+            NetUser myUser = (from x in dbcon.NetUsers
+                                    where x.UserName == HttpContext.Current.Session["nUserName"].ToString()
+                                    && x.UserPassword == HttpContext.Current.Session["uPass"].ToString()
+                                    select x).First();
+
+            if (myUser != null)
+            {
+                //Add UserID and User type to the Session
+                HttpContext.Current.Session["userID"] = myUser.UserID;
+                HttpContext.Current.Session["userType"] = myUser.UserType;
+
+            }
+            if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Member")
+            {
+
+                FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
+
+                Response.Redirect("~/KRMembers/memberpage.aspx");
+            }
+            else if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Instructor")
+            {
+
+                FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
+
+                Response.Redirect("~/KRInstructors/instructorpage.aspx");
+            }
+            else if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Administrator")
+            {
+
+                FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
+
+                Response.Redirect("~/KRAdministrators/adminpage.aspx");
+            }
+            else
+                Response.Redirect("Logon.aspx", true);
+
+
 
         }
     }
